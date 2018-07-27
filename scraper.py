6d114dca61b32f62b4aa3f0c1a1f70b0710d56bb
Bernie Chiu <bernie_chiu@hotmail.com>
2018-07-27 21:14:07 +0800

    Process app scraper and output to JSON

--- scraper.py
+++ scraper.py
@@ -2,7 +2,9 @@ from scrapy.crawler import CrawlerProcess
 from app_scraper.spider import AppStoreScraper
 
 process = CrawlerProcess({
-    'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)'
+    'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)',
+    'FEED_FORMAT': 'json',
+    'ITEM_PIPELINES': {'app_scraper.pipelines.JsonPipeline': 0}
 })
 process.crawl(AppStoreScraper)
 process.start()
